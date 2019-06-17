a83f7f97000509035c3fb6eebf4437dab04b5f56
Selim Chiki <selimchiki@yahoo.fr>
2019-06-17 13:55:22 +0200

    Initial Commit

---
+++ GithubResearch/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GithubResearch
+//
+//  Created by Sélim Chiki on 17/06/2019.
+//  Copyright © 2019 Sélim Chiki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
