984671064c3897bd62bc2198b8aabb3859106c98
Selim Chiki <selimchiki@yahoo.fr>
2019-06-17 14:33:43 +0200

    Create controller for GithubService

---
+++ GithubResearch/Controller/GithubService.swift
@@ -0,0 +1,47 @@
+//
+//  GithubService.swift
+//  GithubResearch
+//
+//  Created by Sélim Chiki on 17/06/2019.
+//  Copyright © 2019 Sélim Chiki. All rights reserved.
+//
+
+import Foundation
+
+class GithubService {
+    private let session: URLSession
+    private let decoder: JSONDecoder
+    
+    init(session: URLSession = .shared, decoder: JSONDecoder = .init()) {
+        self.session = session
+        self.decoder = decoder
+    }
+    
+    func search(matching query: String, handler: @escaping (Result<[Repo], Error>) -> Void) {
+        guard var urlComponents = URLComponents(string: "https://api.github.com/search/repositories") else {
+            preconditionFailure("Can't create url components...")
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "q", value: query)
+        ]
+        
+        guard let url = urlComponents.url else {
+            preconditionFailure("Can't create url from url components...")
+        }
+        
+        session.dataTask(with: url) { [weak self] data, _, error in
+            if let error = error {
+                handler(.failure(error))
+            } else {
+                do {
+                    let data = data ?? Data()
+                    let response = try? self?.decoder.decode(SearchResponse.self, from: data)
+                    handler(.success(response?.items ?? []))
+                } catch {
+                    handler(.failure(error))
+                }
+            }
+        }.resume()
+    }
+}
